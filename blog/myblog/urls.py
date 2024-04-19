2c02f22b3f50707b9a2d336bbc149a7b4142785d
OrlandoQ03 <orlandoleo.quintana@calbaptist.edu>
2024-04-18 22:37:45 -0700

    Added urls and html templates

--- blog/myblog/urls.py
+++ blog/myblog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.blog_index, name="blog_index"),
+    path("create/", views.blog_create, name="blog_create"),
 ]
