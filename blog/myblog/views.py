e6109975b6a5e3da5ed0652c34626fdfc3bcde2e
OrlandoQ03 <orlandoleo.quintana@calbaptist.edu>
2024-04-18 21:57:18 -0700

    Edited py files

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello World")
+    return render(request, "home.html")
+
+def blogpost(request):
+    return render(request, "blogpost.html")
