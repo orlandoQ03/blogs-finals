d8c098991ea383677f8aa25afa279da56be65eaa
OrlandoQ03 <orlandoleo.quintana@calbaptist.edu>
2024-04-20 19:24:21 -0700

    Added delete feature

--- blog/myblog/urls.py
+++ blog/myblog/urls.py
@@ -4,4 +4,5 @@ from .import views
 urlpatterns = [
     path("", views.blog_index, name="blog_index"),
     path("create/", views.blog_create, name="blog_create"),
+    path('delete/<int:id>/', views.blog_delete, name='blog_delete'),
 ]
--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import BlogPost
 from django.contrib import messages
 
@@ -19,12 +19,26 @@ def blog_create(request):
             post.save()
             return redirect("blog_index")
         else:
-            messages.error(request, "Title or Content is blank.")
-
+            messages.error(request, "Please fill in all fields.")
     return render(request, "blog_create.html")
+
+def blog_delete(request, id):
+    post = get_object_or_404(BlogPost, id=id)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('blog_index')
+    return render(request, 'blog_delete.html', {'post': post})
 
 def home(request):
     return render(request, "home.html")
 
 def blogpost(request):
     return render(request, "blogpost.html")
+
+# TODO
+#     Implement CRUD: Create [done], Read [done], Update, Delete[done]
+#     Add more styles to the blog
+#     add commenting feature
+#     fix the post view
+#     ????
+#     profit
